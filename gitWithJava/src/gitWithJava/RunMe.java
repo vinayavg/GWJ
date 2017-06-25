85947efd2ea7f09b2fc4d60db2c60a94288ff9fb
Vinay Aaraga Vishnumurthy <vinay.vishnumurthy@wirpo.com>
2017-06-25 21:23:57 +0530

    Ini commit to vinGit

---
+++ gitWithJava/src/gitWithJava/RunMe.java
@@ -0,0 +1,19 @@
+package gitWithJava;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RunMe {
+
+	@Test
+	public void test() {
+		System.out.println("Print kinda HW from Junit");
+		if(2!=2)
+		{
+			fail("Not yet implemented");
+		}
+		
+	}
+
+}
