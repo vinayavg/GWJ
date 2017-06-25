5874278074b77cd4ed2d5f56f426b79590f5052b
vinayavg <reach.vinay.av01@gmail.com>
2017-06-25 22:37:16 +0530

    Update RunMe.java

--- gitWithJava/src/gitWithJava/RunMe.java
+++ gitWithJava/src/gitWithJava/RunMe.java
@@ -8,7 +8,7 @@ public class RunMe {
 
 	@Test
 	public void test() {
-		System.out.println("Print kinda HW from Junit");
+		System.out.println("Print kinda HW from Junit!!");
 		if(2!=2)
 		{
 			fail("Not yet implemented");
